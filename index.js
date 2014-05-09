c892401f7221c11409c46fca904439c393a10138
bz <bz@datenkueche.com>
2014-05-09 13:44:14 +0200

    Update index.js
    Cookie is set, I dont't want secret data in the url

--- index.js
+++ index.js
@@ -162,9 +162,11 @@ var sendResponse = function(ctx, err, session) {
             redirectURL.query.error = err;
         } else {
             // append user + session id to the redirect url
+            /* Cookie is set, I dont't want secret data in the url
             redirectURL.query.success = true;
             redirectURL.query.sid = session.id;
             redirectURL.query.uid = session.uid;
+            */
         }
 
         var redirectURLString = '';
